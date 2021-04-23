5bc1fb31d92f35fbaee747833d8e4ad70958ba6e
Bahram Bayramli <bahramnb@code.edu.az>
2021-04-23 21:37:35 +0400

    added owl plujin for owl two rows

--- scripts/script-runner.js
+++ scripts/script-runner.js
@@ -19,3 +19,25 @@
     },
 });
 
+// $(".owl-media").owlCarousel({
+//     loop: true,
+//     autoplay: true,
+//     autoplayHoverPause: true,
+//     autoplayTimeout: 5000,
+//     dots: false,
+//     autoplaySpeed: 2000,
+//     margin:10,
+//     responsive: {
+//         0: {
+//             items: 1,
+//             nav: false,
+//             dots: false
+//         },
+//         768: {
+//             items: 4,
+//             nav: true,
+//             dots: false,
+//
+//         }
+//     },
+// });
