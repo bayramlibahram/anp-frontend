e27e6657c1f5cdb13f192515961486b3dc13801a
Bahram Bayramli <bahramnb@code.edu.az>
2021-04-23 17:01:24 +0400

    update index.html

--- scripts/script-runner.js
+++ scripts/script-runner.js
@@ -1,121 +1,21 @@
-﻿$(".owl-right-top").owlCarousel({
+﻿$(".owl-main").owlCarousel({
     loop: true,
     autoplay: true,
     autoplayHoverPause: true,
-    autoplayTimeout: 6000,
-    items: 1,
-    dots: false,
-    nav: false,
-    autoplaySpeed: 2000
-
-});
-
-$(".owl-right-bottom").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplayTimeout: 7000,
-    items: 1,
+    autoplayTimeout: 5000,
     dots: false,
     autoplaySpeed: 2000,
-});
-
-$(".owl-container").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplayTimeout: 4500,
-    items: 1,
-    margin: 5,
-    dots: true,
-    nav: false,
-    autoplaySpeed: 2000
-
-});
-
-$(".owl-video").owlCarousel({
-    loop: true,
-    autoplay: false,
-    autoplayHoverPause: true,
-    autoplayTimeout: 4500,
     responsive: {
         0: {
             items: 1,
+            nav: false,
+            dots: false
         },
         768: {
-            items: 4,
-
-        }
-    },
-    margin: 5,
-    dots: false,
-    nav: false,
-    autoplaySpeed: 2000
-
-});
-
-$(".owl-writters").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplayTimeout: 4500,
-    responsive: {
-        0: {
             items: 1,
-        },
-        768: {
-            items: 6,
-
+            nav: false,
+            dots: true
         }
     },
-    margin: 5,
-    dots: false,
-    nav: false,
-    autoplaySpeed: 2000
-
 });
-
-$(".owl-partners").owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplayTimeout: 4500,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        768: {
-            items: 5,
-
-        }
-    },
-    margin: 5,
-    dots: false,
-    nav: false,
-    autoplaySpeed: 2000
-
-});
-
-function activateNiceScroll() {
-    if (document.body.offsetWidth > 768) {
-
-        $("#latestpostlist").niceScroll({
-            cursorcolor: "#ca0000",
-            cursorwidth: "10px",
-            cursoropacitymax: 0.7
-        });
-    }
-}
-
-function funcsForBodyResize() {
-   
-    activateNiceScroll();
-    
-}
-
-$(document).ready(function () {
-    window.onresize = funcsForBodyResize;
-    funcsForBodyResize();
-});
-
 
